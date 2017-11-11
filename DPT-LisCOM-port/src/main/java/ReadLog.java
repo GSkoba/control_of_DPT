ac9ca90031b2b1235e0beb3ec0ca844255b6ad90
Grigoriy Skobelev <gskobelevff@gmail.com>
2017-11-11 04:14:02 +0300

    Add files via upload

--- DPT-LisCOM-port/src/main/java/ReadLog.java
+++ DPT-LisCOM-port/src/main/java/ReadLog.java
@@ -1,40 +1,59 @@
-import jssc.SerialPort;
-import jssc.SerialPortEvent;
-import jssc.SerialPortEventListener;
-import jssc.SerialPortException;
+import jssc.*;
+import java.util.Scanner;
 
 public class ReadLog {
 
     private static SerialPort serialPort;
+    private static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        serialPort = new SerialPort("COM3");
+        String[] portNames = SerialPortList.getPortNames();
+        System.out.println("Availible ports:");
+        for(int i = 0; i < portNames.length; i++){
+            System.out.println(portNames[i]);
+        }
 
+        serialPort = new SerialPort("COM3"); // yot can change it
+
+        System.out.println("Write hex here (like '55' or '4f': ");
         try {
             serialPort.openPort();
-            serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8,
-                    SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-            serialPort.setEventsMask(SerialPort.MASK_RXCHAR);
-            serialPort.addEventListener(new EventListener());
-        }catch (SerialPortException ex){
-            System.out.println(ex);
-        }
+            serialPort.setParams(
+                    SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_4,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE
+            );
 
+            serialPort.addEventListener(new EventListener(), SerialPort.MASK_RXCHAR);
+
+            int num = Integer.parseInt(in.next(), 16);
+            while (num != 0x00) {
+                System.out.println("Writing: " + num);
+                serialPort.writeInt(num);
+
+                num = Integer.parseInt(in.next(), 16);
+            }
+            serialPort.closePort();
+        } catch (SerialPortException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static class EventListener implements SerialPortEventListener{
+    private static class EventListener implements SerialPortEventListener {
+        public void serialEvent(SerialPortEvent event) {
+            if(event.isRXCHAR() && event.getEventValue() > 0){
+                try {
+                    int[] data = serialPort.readIntArray();
 
-        public void serialEvent(SerialPortEvent event){
-            if(event.isRXCHAR() && event.getEventValue()>0)
-            try{
-                String data = serialPort.readString(event.getEventValue());
-                System.out.println(data);
-            }catch (SerialPortException ex){
-                System.out.println(ex);
+                    for (int i : data) {
+                        System.out.println("Answer:" + i);
+                    }
+
+                } catch (SerialPortException ex) {
+                    System.out.println(ex);
+                }
             }
         }
-
-
     }
 }
-
